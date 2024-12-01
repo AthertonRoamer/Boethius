997843f1032150dcfdb4a248a469d60a6c64ff9e
Schmo103 <112767599+Schmo103@users.noreply.github.com>
2024-12-01 17:08:26 -0600

    fixed hud

--- boethius/game/game.gd
+++ boethius/game/game.gd
@@ -6,15 +6,14 @@ extends Node
 #this is THE game
 func get_active_level() -> Level:
 	return level_manager.active_level
-	
-	
+
+
 func get_command_mode() -> CommandMode:
 	if get_active_level() == null:
 		return null
 	else:
 		return get_active_level().command_mode
-		
-		
+	
 func _ready() -> void:
 	Hud.activate()
 	
